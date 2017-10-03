531d12c8ba2432c8c855e2a2bf4ba77b4e59d5e8
Lucas Correa <lucasccorrea@gmail.com>
2017-10-03 12:21:45 -0300

    get picture user on login

--- src/ion.facebook.provider.ts
+++ src/ion.facebook.provider.ts
@@ -8,13 +8,28 @@ export class IonFacebookProvider {
   private _listPermissions: string[];
 
   constructor(private fb: Facebook, @Inject('permissions') private permissions: string[]) {
-    this._listPermissions = permissions || ['user_friends', 'email', 'public_profile'];
+    this._listPermissions = permissions || ['email', 'public_profile'];
   }
 
-  public login(): Promise<FbLoginResponse> {
+  public login(getPictureBase64?: boolean): Promise<FbLoginResponse> {
     return this.fb.login(this.listPermisions())
       .then((res: FacebookLoginResponse) => this._getLoginResponse(res))
+      .then(res => {
+        if (!getPictureBase64) {
+          return res;
+        } else {
+          return this._handleUserPicture(res);
+        }
+      })
       .catch(error => this._handleErrorLogin(error));
+  }
+
+  public logout(): Promise<any> {
+    return this.fb.logout();
+  }
+
+  public getPictureUser() {
+    return this.requestDataByGraphApi('me/picture/?redirect=false');
   }
 
   public listPermisions(): string[] {
@@ -25,6 +40,63 @@ export class IonFacebookProvider {
     const headers = new Headers();
     headers.append('AcessTokenFb', tokenFb);
     return headers;
+  }
+
+  public getBase64FromPicture(url: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      const image = new Image();
+      image.crossOrigin = 'Anonymous';
+      image.onload = () => {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        let dataURL;
+        canvas.width = image.width; // or 'width' if you want a special/scaled size
+        canvas.height = image.height; // or 'height' if you want a special/scaled size
+        ctx.drawImage(image, 0, 0);
+        dataURL = canvas.toDataURL('image/jpg');
+        resolve(dataURL);
+      };
+      image.onerror = (error) => {
+        reject(error);
+      };
+      image.src = url;
+    });
+  }
+
+  public getPermissions(): Promise<any> {
+    return this.requestDataByGraphApi('/me/permissions')
+      .then(data => this._handleDataPermission(data));
+  }
+
+  public requestDataByGraphApi(path: string, permissions?: string[]) {
+    return this.fb.api(path, permissions);
+  }
+
+  private _handleUserPicture(user: FbLoginResponse): Promise<FbLoginResponse> {
+    return this.getPictureUser()
+      .then(picture => {
+        if (picture && picture.data && picture.data.url) {
+          user.picture = picture.data.url;
+          return this.getBase64FromPicture(user.picture)
+            .then(base64 => {
+              if (base64.startsWith('data:')) {
+                base64 = base64.substring(22, base64.length);
+              }
+              user.picture64 = base64;
+              return user;
+            })
+            .catch((error) => user);
+        } else {
+          return user;
+        }
+      })
+      .catch(error => user);
+  }
+
+  private _handleDataPermission(data: any): {[key: string]: string} {
+    const permissions = {};
+    data.data.forEach(p => permissions[p.permission] = p.status);
+    return permissions;
   }
 
   private _createFbLoginResponse(res: FacebookLoginResponse): FbLoginResponse {
@@ -49,6 +121,10 @@ export class IonFacebookProvider {
     } else {
       throw error;
     }
+  }
+
+  private _getTokenFb(): Promise<string> {
+    return this.fb.getAccessToken();
   }
 
 }
--- src/models/facebook.ts
+++ src/models/facebook.ts
@@ -1,13 +1,7 @@
 import { FacebookLoginResponse } from '@ionic-native/facebook';
 
-export class FacebookAuthorize {
-  email?: string;
-  name?: string;
+export interface FbLoginResponse extends FacebookLoginResponse {
+  header?: any;
   picture?: string;
   picture64?: string;
-  accessToken?: string;
-  userIdFace: string;
-}
-export interface FbLoginResponse extends FacebookLoginResponse {
-  header: any;
 }
